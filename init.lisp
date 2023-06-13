8b62323c3fb348e40ff05dcd020f523069e47c7e
izzy Meyer <izder456@disroot.org>
2023-06-13 18:09:37 -0500

    refactor code

--- init.lisp
+++ init.lisp
@@ -1,50 +1,85 @@
+;; StumpWM configuration file
+
 (in-package :stumpwm)
-;; Message window themeing
-(set-font "xft:GohuFont Nerd Font Mono:size=14")
+
+;; Set font and colors for the message window
+(set-font "xft:GohuFont 14 Nerd Font Mono:style=Regular")
 (set-fg-color "#ebdbb2")
 (set-bg-color "#282828")
 (set-border-color "#ebdbb2")
 (set-msg-border-width 4)
 
+;; Define color variables
 (defvar color1 "#fabd2f")
 (defvar color2 "#282828")
 
+;; Utility functions for displaying system information
+
+;; Run a shell command and format the output
+(defun run-shell-command-and-format (command)
+  (substitute #\Space #\Newline (run-shell-command command t)))
+
+;; Show the kernel version
 (defun show-kernel ()
-  (let ((kernel (run-shell-command "uname -r" t)))
-    (substitute #\Space #\Newline kernel)))
+  (run-shell-command-and-format "uname -r"))
 
+;; Show the hostname
 (defun show-hostname ()
-  (let ((host-name (run-shell-command "hostname" t)))
-    (substitute #\Space #\Newline host-name)))
+  (run-shell-command-and-format "hostname"))
 
+;; Show battery information
 (defun show-battery ()
-  (let ((batt-perc (run-shell-command "battstat -c '++' -d '--' {i} {p}" t)))
-    (substitute #\Space #\Newline batt-perc)))
+  (run-shell-command-and-format "battstat -c '++' -d '--' {i} {p}"))
 
+;; Show the temperature
 (defun show-temp ()
-  (let ((temp-celc (run-shell-command "sysctl -n hw.sensors.acpithinkpad0.temp4" t)))
-    (substitute #\Space #\Newline temp-celc)))
+  (run-shell-command-and-format "sysctl -n hw.sensors.acpithinkpad0.temp4"))
 
-(setf
-  stumpwm:*mode-line-background-color* color2
-  stumpwm:*mode-line-foreground-color* color1
-  stumpwm:*mode-line-border-color* "#ebdbb2"
-  stumpwm:*screen-mode-line-format* (list "%g | %v ^>^7 | " '(:eval (show-hostname)) "| " '(:eval (show-kernel)) "| " '(:eval (show-battery)) "| " '(:eval (show-temp)) "| %d")
-  stumpwm:*mode-line-border-width* 4
-  stumpwm:*mode-line-pad-x* 16
-  stumpwm:*mode-line-pad-y* 14
-  stumpwm:*mode-line-timeout* 5
-  stumpwm:*group-format*  "%n %t"
-  stumpwm:*time-modeline-string* "%a, %B %d %Y @%I:%M%p"
-  stumpwm:*window-format* "^b^(:fg \"#b8bb26\")<%25t>"
-  stumpwm:*window-border-style* :tight
-  stumpwm:*normal-border-width* 4
-  stumpwm:*message-window-padding* 16
-  stumpwm:*message-window-y-padding* 14
-  stumpwm:*message-window-gravity* :bottom-left
-  )
+;; Show the window title
+(defun show-window-title ()
+  (substitute #\Space #\Newline (window-title (current-window))))
+
+;; Configure StumpWM appearance settings
+
+;; Mode line settings
+(setf stumpwm:*mode-line-background-color* color2
+      stumpwm:*mode-line-foreground-color* color1
+      stumpwm:*mode-line-border-color* "#ebdbb2"
+      stumpwm:*mode-line-border-width* 4
+      stumpwm:*mode-line-pad-x* 16
+      stumpwm:*mode-line-pad-y* 14
+      stumpwm:*mode-line-timeout* 5)
+
+;; Group format
+(setf stumpwm:*group-format* "%n %t")
+
+;; Time modeline format
+(setf stumpwm:*time-modeline-string* "%a, %B %d %Y @%I:%M%p")
+
+;; Window format
+(setf stumpwm:*window-format* "^b^(:fg \"#b8bb26\")<%25t>")
+(setf stumpwm:*window-border-style* :tight)
+(setf stumpwm:*normal-border-width* 4)
+
+;; Message window settings
+(setf stumpwm:*message-window-padding* 16)
+(setf stumpwm:*message-window-y-padding* 14)
+(setf stumpwm:*message-window-gravity* :bottom-left)
+
+;; Screen mode line format
+(setf stumpwm:*screen-mode-line-format*
+      (list "%g | %v ^>^7 | "
+            '(:eval (show-hostname))
+            "| " '(:eval (show-kernel))
+            "| " '(:eval (show-battery))
+            "| " '(:eval (show-temp))
+            "| %d"))
+
+;; Set focus and unfocus colors
 (stumpwm:set-focus-color "#ebdbb2")
 (stumpwm:set-unfocus-color "#282828")
+
+;; Rename and create new groups
 (stumpwm:grename "Ness")
 (stumpwm:gnewbg "Paula")
 (stumpwm:gnewbg "Jeff")
@@ -53,60 +88,53 @@
 (stumpwm:gnewbg "Ana")
 (stumpwm:gnewbg "Loid")
 (stumpwm:gnewbg "Teddy")
+
+;; Toggle mode line display
 (stumpwm:toggle-mode-line (stumpwm:current-screen) (stumpwm:current-head))
 
-;; Set Prefix
+;; Set prefix key
 (set-prefix-key (kbd "C-t"))
-(define-key *top-map* (kbd "M-ESC") "mode-line")
+
+;; Define keybindings
+
+;; Global keybindings
+(define-key *root-map* (kbd "M-ESC") "mode-line")
 (define-key *root-map* (kbd "M-q") "quit")
 
-;; prompt the user for an interactive command. The first arg is an
-;; optional initial contents.
-(defcommand colon1 (&optional (initial "")) (:rest)
-  (let ((cmd (read-one-line (current-screen) ": " :initial-input initial)))
-    (when cmd
-      (eval-command cmd t))))
-
-;; Browser
+;; Application and command keybindings
 (define-key *root-map* (kbd "f") "exec qutebrowser")
-;; Term
 (define-key *root-map* (kbd "c") "exec st")
 (define-key *root-map* (kbd "C-c") "exec st")
-;; FileMgr
-(define-key *root-map* (kbd "F") "exec caja" )
-;; Rofi Menu
+(define-key *root-map* (kbd "F") "exec caja")
 (define-key *root-map* (kbd "space") "exec rofi -i -show drun -modi drun -show-icons")
-;; BG Set
 (define-key *root-map* (kbd "M-b") "exec feh --bg-fill $(shuf -n1 -e /usr/local/share/backgrounds/*)")
-;; Spotify
 (define-key *root-map* (kbd "P") "exec st -T ncspot -e ncspot")
-;; htop
 (define-key *root-map* (kbd "i") "exec st -T htop -e htop")
-;; neovim
-(define-key *root-map* (kbd "e") "exec st -T nvim -e nvim")
-;; XKill
+(define-key *root-map* (kbd "e") "exec emacsclient -c -a 'emacs'")
 (define-key *root-map* (kbd "x") "exec xkill")
 
-;; Handle Window movine between groups)
+;; Window movement between groups
 (define-key *root-map* (kbd "m") "mark")
 (define-key *root-map* (kbd "M") "gmove-marked")
 
-;; special-Keys
+;; Special keys
 (define-key *top-map* (kbd "Print") "exec scrot -F ~/Pictures/screenshot-`date +%F`.png")
 (define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec sndioctl output.level=+0.05")
 (define-key *top-map* (kbd "XF86AudioLowerVolume") "exec sndioctl output.level=-0.05")
 (define-key *top-map* (kbd "XF86AudioMute") "exec sndioctl output.mute=!")
 
-;; Web jump (works for DuckDuckGo and Imdb)
+;; Web jump commands
 (defmacro make-web-jump (name prefix)
-  `(defcommand ,(intern name) (search) ((:rest ,(concatenate 'string name " search: ")))
-    (nsubstitute #\+ #\Space search)
-    (run-shell-command (concatenate 'string ,prefix search))))
+  `(defcommand ,(intern name) (search)
+     ((:rest ,(concatenate 'string name " search: ")))
+     (nsubstitute #\+ #\Space search)
+     (run-shell-command (concatenate 'string ,prefix search))))
 
 (make-web-jump "linxer" "qutebrowser https://linxer.org/search.php?q=")
 (make-web-jump "ddg" "qutebrowser https://lite.duckduckgo.com/lite?q=")
 (make-web-jump "whoogle" "qutebrowser https://whoogle.click/search?q=")
-;; C-t M-s is a terrble binding, but you get the idea.
+
+;; Keybindings for web jumps
 (define-key *root-map* (kbd "M-s") "linxer")
 (define-key *root-map* (kbd "M-d") "ddg")
 (define-key *root-map* (kbd "M-w") "whoogle")
