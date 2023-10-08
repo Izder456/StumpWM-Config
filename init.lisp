32564ce1212586ae0778730c643afed4841d6644
izder456 <izder456@disroot.org>
2023-10-08 01:10:53 -0500

    add contrib repo, add gaps, and more boilerplate

--- init.lisp
+++ init.lisp
@@ -1,10 +1,39 @@
+;; Quicklisp Setup
+(let ((quicklisp-init (merge-pathnames ".quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
 ;; this automatically prefixes 'stumpwm:' to commands that need it
 (in-package :stumpwm)
+(setf *default-package* :stumpwm)
+
+;; Set Modules
+(set-module-dir "/home/izder456/.stumpwm.d/modules")
+
+;; Colors Gruv Super List
+(defvar iz-black "#282828")
+(defvar iz-red "#CC241D")
+(defvar iz-softred "#FB4934")
+(defvar iz-green "#98971A")
+(defvar iz-softgreen "#B8BB26")
+(defvar iz-yellow "#D79921")
+(defvar iz-softyellow "#FABD2F")
+(defvar iz-blue "#458588")
+(defvar iz-softblue "#83A598")
+(defvar iz-purple "#B16286")
+(defvar iz-softpurple "#D3869B")
+(defvar iz-aqua "#689D6A")
+(defvar iz-softaqua "#8EC07C")
+(defvar iz-orange "#D65D0E")
+(defvar iz-softorange "#FE8019")
+(defvar iz-white "#EBDBB2")
+(defvar iz-gray "#928374")
 
 ;; Set font and colors for the message window
-(set-fg-color "#ebdbb2")
-(set-bg-color "#282828")
-(set-border-color "#ebdbb2")
+(set-fg-color iz-white)
+(set-bg-color iz-black)
+(set-border-color iz-white)
 (set-msg-border-width 4)
 (set-font "-misc-spleen-medium-r-normal--16-160-72-72-c-80-iso10646-1")
 (setf *mouse-focus-policy* :click)
@@ -15,41 +44,60 @@
 (setf (getenv "GDK_CORE_DEVICE_EVENTS") "1")
 (setf (getenv "PAGER") "most")
 (setf (getenv "TERM") "xterm-256color")
+(setf (getenv "PKG_PATH") "https://cdn.OpenBSD.org/pub/OpenBSD/snapshots/packages/amd64")
+
+;; Set Gaps
+(load-module "swm-gaps")
+
+;; Head gaps run along the 4 borders of the monitor(s)
+;; (setf swm-gaps:*head-gaps-size* 6)
+
+;; Inner gaps run along all the 4 borders of a window
+(setf swm-gaps:*inner-gaps-size* 10)
+
+;; Outer gaps add more padding to the outermost borders of a window (touching the screen border)
+(setf swm-gaps:*outer-gaps-size* 12)
+
+;; Turn em on
+(swm-gaps:toggle-gaps)
 
 ;; Mode line settings
-(setf stumpwm:*mode-line-background-color* "#282828"
-      stumpwm:*mode-line-foreground-color* "#fabd2f"
-      stumpwm:*mode-line-border-color* "#ebdbb2"
-      stumpwm:*mode-line-border-width* 4
-      stumpwm:*mode-line-pad-x* 16
-      stumpwm:*mode-line-pad-y* 14
-      stumpwm:*mode-line-timeout* 5)
+(setf *mode-line-background-color* iz-black
+      *mode-line-foreground-color* iz-softyellow
+      *mode-line-border-color* iz-white
+      *mode-line-border-width* 4
+      *mode-line-pad-x* 16
+      *mode-line-pad-y* 14
+      *mode-line-timeout* 5)
 
 ;; Set focus and unfocus colors
-(stumpwm:set-focus-color "#ebdbb2")
-(stumpwm:set-unfocus-color "#282828")
+(set-focus-color iz-white)
+(set-unfocus-color iz-gray)
 
 ;; Rename and create new groups
-(stumpwm:grename "Ness")
-(stumpwm:gnewbg "Paula")
-(stumpwm:gnewbg "Jeff")
-(stumpwm:gnewbg "Poo")
+(grename "Ness")
+(gnewbg "Paula")
+(gnewbg "Jeff")
+(gnewbg "Poo")
 
 ;; Group format
-(setf stumpwm:*group-format* "%n %t")
+(setf *group-format* "%n %t")
 
 ;; Time modeline format
-(setf stumpwm:*time-modeline-string* "%a, %b%d @%I:%M%p")
+(setf *time-modeline-string* "%a, %b%d @%I:%M%p")
 
 ;; Window format
-(setf stumpwm:*window-format* "^b^(:fg \"#b8bb26\")<%25t>")
-(setf stumpwm:*window-border-style* :tight)
-(setf stumpwm:*normal-border-width* 4)
+(setf *window-format* (format NIL "^b^(:fg \"~A\")<%25t>" iz-softgreen))
+(setf *window-border-style* :tight)
+(setf *normal-border-width* 4)
 
 ;; Message window settings
-(setf stumpwm:*message-window-padding* 16)
-(setf stumpwm:*message-window-y-padding* 14)
-(setf stumpwm:*message-window-gravity* :bottom-left)
+(setf *message-window-padding* 16)
+(setf *message-window-y-padding* 14)
+(setf *message-window-gravity* :center)
+
+;; Input window settings
+(setf *input-window-gravity* :center)
 
 ;; Run a shell command and format the output
 (defun run-shell-command-and-format (command)
@@ -85,7 +133,7 @@
             "| %d"))
 
 ;; Toggle mode line display
-(stumpwm:toggle-mode-line (stumpwm:current-screen) (stumpwm:current-head))
+(toggle-mode-line (current-screen) (current-head))
 
 ;; Load BIND file
 (load "~/.stumpwm.d/bind.lisp")
